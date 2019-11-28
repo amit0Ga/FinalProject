f5c230ce2498b9bea69ea9b245515cef2fcd2f5d
ron981 <34375787+ron981@users.noreply.github.com>
2019-11-28 16:09:33 +0200

    json library was added succefully

--- src/Utilities.java
+++ src/Utilities.java
@@ -6,60 +6,88 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import org.json.*;
-
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Utilities {
-	public static void main (String args[])
-	{
-		String resultsApi = "https://apiv2.apifootball.com/?action=get_events&from=$&to=*&league_id=468&APIkey=c8d7070a793a64e2caa3f0977247fcede6e90a5a38ac5a3f85963291ed7ecfc6";		
+	public static void main(String args[]) {
+		String resultsApi = "https://apiv2.apifootball.com/?action=get_events&from=$&to=*&league_id=468&APIkey=c8d7070a793a64e2caa3f0977247fcede6e90a5a38ac5a3f85963291ed7ecfc6";
 		try {
 			readUrl(prepareResultsUrlTwoMonthsAgo(resultsApi));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//JSONParser parse = new JSONParser();
-		
-	}
-	private static String readUrl(String urlString) throws Exception {
-	    BufferedReader reader = null;
-	    try {
-	        URL url = new URL(urlString);
-	        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-	        reader = new BufferedReader(new InputStreamReader(url.openStream()));
-	        StringBuffer buffer = new StringBuffer();
-	        int read;
-	        char[] chars = new char[1024];
-	        while ((read = reader.read(chars)) != -1)
-	            buffer.append(chars, 0, read); 
+		// JSONParser parse = new JSONParser();
 
-	        return buffer.toString();
-	    } finally {
-	        if (reader != null)
-	            reader.close();
-	    }
 	}
-	private static String prepareResultsUrlTwoMonthsAgo(String urlString)
-	{
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); 	// In order to take to two last months.
-        Date date = new Date();
-        String today = dateFormat.format(date).toString();
-        int x = Integer.parseInt(today.substring(5, 7)) -2; // 5,7 refers to the month field, -2 refers to two months later.
-        
-        switch(x)	// Try to change to a more elegant code
-        {
-    	case 0:
-    		x=12;
-    		break;
-    		
-    	case -1:
-    		x=11;
-    		break;
-        }
-        String twoMonthAgo = today.replace(today.substring(5, 7),String.valueOf(x)); 
-        urlString = urlString.replace("$", twoMonthAgo);
-        urlString = urlString.replace("*", today);
-        System.out.println(urlString);
-        return urlString;
+
+	private static String readUrl(String urlString) throws Exception {
+		BufferedReader reader = null;
+		try {
+			/*
+			 * URL url = new URL(urlString); HttpURLConnection conn =
+			 * (HttpURLConnection)url.openConnection(); reader = new BufferedReader(new
+			 * InputStreamReader(url.openStream())); StringBuffer buffer = new
+			 * StringBuffer(); int read; char[] chars = new char[1024]; while ((read =
+			 * reader.read(chars)) != -1) buffer.append(chars, 0, read);
+			 * 
+			 * return buffer.toString();
+			 */
+			HttpURLConnection con;
+			URL url = new URL(urlString);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.connect();
+			int responsecode = con.getResponseCode();
+			if (responsecode != 200)
+				throw new RuntimeException("HttpResponseCode: " + responsecode);
+
+			else {
+				Scanner sc = new Scanner(url.openStream());
+				String inline = "";
+				while (sc.hasNext()) {
+					inline += sc.hasNextLine();
+				}
+				System.out.println(inline);
+				JSONParser J = new JSONParser();
+
+				JSONObject jobj = (JSONObject) J.parse(inline);
+				JSONArray jsonarr_1 = (JSONArray) jobj.get("team_key");
+				// System.out.println(jsonarr_1);
+				return inline;
+			}
+
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+
+	private static String prepareResultsUrlTwoMonthsAgo(String urlString) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // In order to take to two last months.
+		Date date = new Date();
+		String today = dateFormat.format(date).toString();
+		int x = Integer.parseInt(today.substring(5, 7)) - 2; // 5,7 refers to the month field, -2 refers to two months
+																// later.
+
+		switch (x) // Try to change to a more elegant code
+		{
+		case 0:
+			x = 12;
+			break;
+
+		case -1:
+			x = 11;
+			break;
+		}
+		String twoMonthAgo = today.replace(today.substring(5, 7), String.valueOf(x));
+		urlString = urlString.replace("$", twoMonthAgo);
+		urlString = urlString.replace("*", today);
+		System.out.println(urlString);
+
+		return urlString;
+
 	}
 }
